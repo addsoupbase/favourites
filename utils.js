329ca202a678f5f5dcc8b27fba6e8adefcca7d2f
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2024-10-18 22:17:21 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -13,50 +13,40 @@ cwebp file.png -o file.webp
 */
 
 const ran = {
-    choose: (...a) => a[Math.floor(Math.random() * a.length)],
-    range: (min, max) => Math.random() * (max - min) + min,
-    frange: (min, max) => Math.floor(ran.range(min, max)),
-    pseudo: () => `${Date.now()}`.at(-1) / 10,
-    true: () => crypto.getRandomValues(new Uint32Array(1))[0] / 4_294_967_295,
+    choose:(...a)=>a[Math.floor(Math.random() * a.length)],
+    range:(min, max)=>Math.random() * (max - min) + min,
+    frange:(min, max)=>Math.floor(ran.range(min, max)),
+    pseudo:_=>`${Date.now()}`.at(-1)/10,
+    true:_=>crypto.getRandomValues(new Uint32Array(1))[0]/4_294_967_295,
     shuffle: (...item) => {
-        let n = item.length
-        let ammo = [...Array(n).keys()] // Create an array with indices [0, 1, 2, ..., n-1]
-        let out = []
-        while (ammo.length) {
-            let randIndex = Math.floor(Math.random() * ammo.length)
-            let chosenIndex = ammo[randIndex]
-            out.push(item[chosenIndex])
-            ammo.splice(randIndex, 1) // Remove the used index from the ammo array
+        for (let i = item.length - 1; i > 0; --i) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [item[i], item[j]] = [item[j], item[i]]; // Swap elements
         }
-        return out
-    },
+        return item;
+    },    
     gen(len = 6) {
-        let str
-        do {
-            str = ''
-            for (let i = len; i--;) str += ran.choose(...utilString._alphabet + utilString._numbers + utilString._ALPHABET)
-        } while (ran.gen.previouslygenerated.has(str))
+        let pool=utilString._alphabet+utilString._numbers+utilString._ALPHABET,poolSize=pool.length,str;
+        do str=Array.from({ length: len },_=>pool[Math.floor(Math.random()*poolSize)]).join``;
+        while (ran.gen.previouslygenerated.has(str))
         ran.gen.previouslygenerated.add(str)
         return str
     },
-    Randomizer: class Randomizer {
-        constructor(num = 6) {
+     Randomizer:function(n=6) {
             return new Proxy({}, {
-                get(target, prop) {
-                    return target[prop] ??= ran.gen(num)
-                }
-            })
-        }
+                get:(t, p)=>t[p]??=ran.gen(n)
+            });
     }
+    
 }
 ran.gen.previouslygenerated = new Set
 const utilMath = {
-    isInt: n => Math.trunc(n) === n,
-    sanitize: num => (num == num) && num != null && isFinite(num),
+    isInt:n=>Math.trunc(n)==n,
+    sanitize:num=>(num==num)&&num!=null&&isFinite(num),
     equality: (...target) => target.every(o => Object.is(o, target[0])),
     arreq(...targets) {
         if (targets.length < 2) throw RangeError('At least 2 arguments required.')
-        for (let i = 0, max = Math.max(...targets.map(o => o.length)) || 1; i < max; i++) {
+        for (let i = 0, max = Math.max(...targets.map(o => o.length)) || 1; i < max; ++i) {
             if (!Array.isArray(targets[i])) throw TypeError(`arguments[${i}] is not an Array (${typeof arguments[i]})`)
             for (let comparisons of targets) if (!utilMath.equality(targets[0][i], comparisons[i])) return false
         }
@@ -65,20 +55,15 @@ const utilMath = {
     toRad: deg => deg * Math.PI / 180,
     toDeg: rad => rad * 180 / Math.PI,
     diff: (a, b) => Math.abs(a - b),
-    clamp(val, min, max) { if (val > max) return max; if (val < min) return min; return val },
-    Cycle: class Cycle {
-        constructor(...items) {
-            return Object.defineProperty(function* (t) {
-                let x = 0
-                for (; ;) {
-                    if (x === t.length) x = 0
-                    yield t[x++]
-                }
-            }(items), 'val', {
-                get() { return this.next().value },
-            })
-        }
-    },
+    clamp(val, min, max){if(val>max)return max;if(val<min)return min;return val},
+     Cycle: function(...items) {
+            return Object.defineProperty(function*(x=0) {
+            for(;;)yield items[x++%items.length]
+            }(), 'val', {
+                get(){return this.next().value},
+            });
+    }
+    
 
 
 }
@@ -955,6 +940,7 @@ class Elem {
         }, callback)        // this.anim({ class: 'fade out' }, () => { this.styleMe({opacity:0}); callback?.call?.(this) })
     }
     async fadeIn(callback) {
+        this.styleMe({opacity:0})
         return this.transition({
             frames: { opacity: 1 }, timing: { duration: 300 },
         }, callback)
@@ -1100,11 +1086,10 @@ class SceneryElem extends Elem {
 }
 const color = Object.defineProperties((j =>
     "aliceblue&#f0f8ff&antiquewhite&#faebd7&aqua&#00ffff&aquamarine&#7fffd4&azure&#f0ffff&beige&#f5f5dc&bisque&#ffe4c4&black&#000000&blanchedalmond&#ffebcd&blue&#0000ff&blueviolet&#8a2be2&brown&#a52a2a&burlywood&#deb887&cadetblue&#5f9ea0&chartreuse&#7fff00&chocolate&#d2691e&coral&#ff7f50&cornflowerblue&#6495ed&cornsilk&#fff8dc&crimson&#dc143c&cyan&#00ffff&darkblue&#00008b&darkcyan&#008b8b&darkgoldenrod&#b8860b&darkgray&#a9a9a9&darkgreen&#006400&darkkhaki&#bdb76b&darkmaran.genta&#8b008b&darkolivegreen&#556b2f&darkorange&#ff8c00&darkorchid&#9932cc&darkred&#8b0000&darksalmon&#e9967a&darkseagreen&#8fbc8f&darkslateblue&#483d8b&darkslategray&#2f4f4f&darkturquoise&#00ced1&darkviolet&#9400d3&deeppink&#ff1493&deepskyblue&#00bfff&dimgray&#696969&dodgerblue&#1e90ff&firebrick&#b22222&floralwhite&#fffaf0&forestgreen&#228b22&fuchsia&#ff00ff&gainsboro&#dcdcdc&ghostwhite&#f8f8ff&gold&#ffd700&goldenrod&#daa520&gray&#808080&grey&#808080&green&#008000&greenyellow&#adff2f&honeydew&#f0fff0&hotpink&#ff69b4&indianred&#cd5c5c&indigo&#4b0082&ivory&#fffff0&khaki&#f0e68c&lavender&#e6e6fa&lavenderblush&#fff0f5&lawngreen&#7cfc00&lemonchiffon&#fffacd&lightblue&#add8e6&lightcoral&#f08080&lightcyan&#e0ffff&lightgoldenrodyellow&#fafad2&lightgray&#d3d3d3&lightgreen&#90ee90&lightpink&#ffb6c1&lightsalmon&#ffa07a&lightseagreen&#20b2aa&lightskyblue&#87cefa&lightslategray&#778899&lightsteelblue&#b0c4de&lightyellow&#ffffe0&lime&#00ff00&limegreen&#32cd32&linen&#faf0e6&maran.genta&#ff00ff&maroon&#800000&mediumaquamarine&#66cdaa&mediumblue&#0000cd&mediumorchid&#ba55d3&mediumpurple&#9370db&mediumseagreen&#3cb371&mediumslateblue&#7b68ee&mediumspringgreen&#00fa9a&mediumturquoise&#48d1cc&mediumvioletred&#c71585&midnightblue&#191970&mintcream&#f5fffa&mistyrose&#ffe4e1&moccasin&#ffe4b5&navajowhite&#ffdead&navy&#000080&oldlace&#fdf5e6&olive&#808000&olivedrab&#6b8e23&orange&#ffa500&orangered&#ff4500&orchid&#da70d6&palegoldenrod&#eee8aa&palegreen&#98fb98&paleturquoise&#afeeee&palevioletred&#db7093&papayawhip&#ffefd5&peachpuff&#ffdab9&peru&#cd853f&pink&#ffc0cb&plum&#dda0dd&powderblue&#b0e0e6&purple&#800080&rebeccapurple&#663399&red&#ff0000&rosybrown&#bc8f8f&royalblue&#4169e1&saddlebrown&#8b4513&salmon&#fa8072&sandybrown&#f4a460&seagreen&#2e8b57&seashell&#fff5ee&sienna&#a0522d&silver&#c0c0c0&skyblue&#87ceeb&slateblue&#6a5acd&slategray&#708090&snow&#fffafa&springgreen&#00ff7f&steelblue&#4682b4&tan&#d2b48c&teal&#008080&thistle&#d8bfd8&tomato&#ff6347&turquoise&#40e0d0&violet&#ee82ee&wheat&#f5deb3&white&#ffffff&whitesmoke&#f5f5f5&yellow&#ffff00&yellowgreen&#9acd32"
-        .split`&`.forEach((a, i) => j += i % 2 ? `"${a}"${i == 283 ? '' : ','}` : `"${a}":`) ?? JSON.parse(`{${j}}`))``, {
-    //Darken Hex Colour
+        .split`&`.forEach((a,i)=>j+=i%2?`"${a}"${i==283?'':','}` : `"${a}":`)??JSON.parse(`{${j}}`))``, {
     dhk: { value(e, f = 40) { let $ = parseInt((e = e.replace(/^#/, "")).substring(0, 2), 16), a = parseInt(e.substring(2, 4), 16), r = parseInt(e.substring(4, 6), 16); return $ = Math.round($ * (1 - f / 100)), a = Math.round(a * (1 - f / 100)), r = Math.round(r * (1 - f / 100)), $ = Math.min(255, Math.max(0, $)), a = Math.min(255, Math.max(0, a)), r = Math.min(255, Math.max(0, r)), "#" + [$, a, r].map(e => { let f = e.toString(16); return 1 === f.length ? "0" + f : f }).join("") }, enumerable: !1 },
-    choose: { value() { return ran.choose(...Object.values(this)) }, enumerable: !1 },
-    log: { value(e) { console.log(`%c ${e}`, `color: ${e};font-size: 100px; background-color: ${e}`) }, enumerable: !1 },
+    choose: { value:_=>ran.choose(...Object.values(color)) , enumerable: !1 },
+    log: { value:e=>console.log(`%c ${e}`, `color: ${e};font-size: 100px; background-color: ${e}`) , enumerable: !1 },
     opposite: { value(e) { if (0 === e.indexOf("#") && (e = e.slice(1)), 3 === e.length && (e = e[0] + e[0] + e[1] + e[1] + e[2] + e[2]), 6 !== e.length) throw Error("Invalid HEX color."); let f = (255 - parseInt(e.slice(0, 2), 16)).toString(16), $ = (255 - parseInt(e.slice(2, 4), 16)).toString(16), a = (255 - parseInt(e.slice(4, 6), 16)).toString(16); return "#" + padZero(f) + padZero($) + padZero(a) }, enumerable: !1 }
 })
 Object.assign(color, {
@@ -1112,5 +1097,5 @@ Object.assign(color, {
 })
 const body = window.body
     ; (n => "escape&unescape&event&external&External&orientation&status&back&blur&captureEvents&clientInformation&clearImmediate&forward&releaseEvents&requestFileSystem&setImmediate&setResizable&showModalDialog&webkitConvertPointFromNodeToPage&webkitConvertPointFromPageToNode&onorientationchange&onunload&vrdisplayactivate&vrdisplayconnect&vrdisplaydeactivate&vrdisplaydisconnect&vrdisplaypresentchange".split('&').forEach(o => delete n[o]))(self)
-    ; (n => "javaEnabled&activeVRDisplays&appCodeName&appName&appVersion&doNotTrack&mimeTypes&oscpu&platform&plugins&product&productSub&vendor&vendorSub&getUserMedia&getVRDisplays&taintEnabled".split('&').forEach(o => delete n[o]))(Navigator.prototype)
+    ;(n=>"javaEnabled&activeVRDisplays&appCodeName&appName&appVersion&doNotTrack&mimeTypes&oscpu&platform&plugins&product&productSub&vendor&vendorSub&getUserMedia&getVRDisplays&taintEnabled".split`&`.map(o=>delete n[o]))(Navigator.prototype)
     ; (n => ['__$Getter__', '__$Setter__'].forEach(o => delete n[o.replace('$', 'define')] & delete n[o.replace('$', 'lookup')]))(Object.prototype)
