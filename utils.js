27129cb270baffdcdc62805c40a2cac60e5f17c8
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2024-12-01 18:05:46 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -232,9 +232,9 @@ const utilArray = (() => {
 {   
     assign(utilString,{
         toOrdinal:y,
-        average: x,
         badwords: RegExp([z(13, 8, 6, 6, 4, 17), z(1, 8, 19, 2, 7), z(5, 20, 2, 10), z(18, 7, 8, 19), z(2, 14, 2, 10), z(5, 0, 6), z(17, 4, 19, 0, 17, 3), z(3, 8, 2, 10)].join('|'))
     })  
+    utilMath.average= x
     const map = new Map([["1", "st"],["2", "nd"],["3", "rd"]])
     , t = 'I love you so much please don\'t disable me 🥺'
     document.addEventListener('DOMContentLoaded', {
